7bc634e3ff1955446ccf24f9739bff9c05a6846f
Gabriele Costanzo <g.costanzo.web@gmail.com>
2020-01-02 16:12:00 +0100

    progetto eclipse creato

---
+++ PCReady-eclipse/PCReady_iterazione1/src/PCReadyMainAppText.java
@@ -0,0 +1,19 @@
+
+/**
+ * @author Bartolomeo Caruso
+ * @author Gabriele Costanzo
+ * @author Giuseppe Fallica
+ *
+ * @version 0.1
+ */
+public class PCReadyMainAppText {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
